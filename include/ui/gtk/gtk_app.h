719951b9fdd9474f4e851840e6ecd96e7082fc05
Rémi Guillette <remiguillette@gmail.com>
2025-10-27 15:21:41 -0400

    Fix GTK navigation for BeaverPhone route

--- include/ui/gtk/gtk_app.h
+++ include/ui/gtk/gtk_app.h
@@ -3,6 +3,7 @@
 #include <gtk/gtk.h>
 #include <webkit2/webkit2.h>
 
+#include <filesystem>
 #include <string>
 
 #include "core/app_manager.h"
@@ -20,7 +21,9 @@ private:
     void load_language(WebKitWebView* web_view, Language language);
     void load_beaverphone(WebKitWebView* web_view, Language language);
     void load_html(WebKitWebView* web_view, const std::string& html);
+    std::string normalize_path(const std::string& raw_path) const;
 
     AppManager& manager_;
     std::string base_uri_;
+    std::filesystem::path base_path_;
 };
